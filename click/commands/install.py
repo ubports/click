ccffe0b5a4994bb3098fb23c2b03e268a9d1751e
Colin Watson <cjwatson@canonical.com>
2013-10-22 11:44:43 +0100

    Adjust top-level "click help" entry for "install" to point to pkcon.

--- click/commands/install.py
+++ click/commands/install.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Install a Click package."""
+"""Install a Click package (low-level; consider pkcon instead)."""
 
 from __future__ import print_function
 

21331d945534cc1fcb097b17d29dc90a2c973bad
Michael Vogt <michael.vogt@ubuntu.com>
2014-06-30 17:24:12 +0200

    make it work for lp:camera-app and lp:notes-app

--- tests/integration/test_build_core_apps.py
+++ tests/integration/test_build_core_apps.py
@@ -15,11 +15,14 @@
 
 """Integration tests for the click chroot feature."""
 
+import json
 import os
+import shutil
 import subprocess
 import unittest
 
 from .helpers import chdir, TestCase
+
 
 @unittest.skipIf(
     os.getuid() != 0, "This tests needs to run as root")
@@ -28,53 +31,76 @@ from .helpers import chdir, TestCase
         ["ping", "-c1", "archive.ubuntu.com"]) != 0, "Need network")
 class TestBuildCoreApps(TestCase):
 
-    ARCH = "armhf"
-    FRAMEWORK = "ubuntu-sdk-14.10"
     TEST_BUILD_BRANCHES = ["lp:camera-app",
+                           "lp:notes-app",
                           ]
-
-    @classmethod
-    def setUpClass(cls):
-        super(TestBuildCoreApps, cls).setUpClass()
-        subprocess.check_call([
-            cls.click_binary,
-            "chroot",
-            "-a", cls.ARCH,
-            "-f", cls.FRAMEWORK,
-            "create"])
-        # bug #1316930(?)
-        subprocess.check_call([
-            cls.click_binary,
-            "chroot",
-            "-a", cls.ARCH,
-            "-f", cls.FRAMEWORK,
-            "maint",
-            "apt-get", "install", "python3",
-        ])
-
-
-    @classmethod
-    def tearDownClass(cls):
-        return subprocess.check_call([
-            cls.click_binary,
-            "chroot", "-a", cls.ARCH,
-            "destroy"])
 
     def _run_in_chroot(self, cmd):
         return subprocess.check_call([
             self.click_binary, "chroot", 
-            "-a", self.ARCH, "-f", self.FRAMEWORK, "run"] + cmd)
+            "-a", self.arch, "-f", self.framework, "run"] + cmd)
 
-    def test_build(self):
+    def _configure(self):
+        self._run_in_chroot(
+            ["cmake", ".", "-DCLICK_MODE=on", "-DINSTALL_TESTS=off"])
+
+    def _make(self):
+        self.assertEqual(self._run_in_chroot(
+            ["make", "DESTDIR=package", "install"]), 0)
+
+    def _build_click(self):
+        subprocess.check_call(
+            [self.click_binary, "build", "package"])
+
+    def _find_manifest(self, start_dir):
+        for path, dirs, files in os.walk(start_dir):
+            for needle in ["manifest.json", "manifest.json.in"]:
+                if needle in files:
+                    return os.path.join(path, needle)
+        return None
+
+    def _set_arch_and_framework_from_manifest(self, manifest):
+        with open(manifest) as f:
+            data = json.load(f)
+        self.arch = data["architecture"]
+        self.framework = data["framework"]
+        # ?!?
+        self.framework = self.framework.rsplit("-", maxsplit=1)[0]
+
+    def _ensure_click_chroot(self):
+        # FIXME: once there is a "click chroot list" we can skip this
+        subprocess.call([
+            self.click_binary,
+            "chroot",
+             "-a", self.arch,
+             "-f", self.framework,
+             "create"])
+        # bug #1316930(?)
+        subprocess.call([
+            self.click_binary,
+            "chroot",
+            "-a", self.arch,
+            "-f", self.framework,
+            "maint",
+            "apt-get", "install", "-y", "python3",
+        ])
+
+    def test_build_core_apps(self):
         for branch in self.TEST_BUILD_BRANCHES:
+            # get and parse
+            branch_dir = branch[len("lp:"):]
+            # always use a fresh checkout
+            if os.path.exists(branch_dir):
+                shutil.rmtree(branch_dir)
             subprocess.check_call(["bzr","branch", branch])
-            with chdir(branch[len("lp:"):]):
-                self.assertEqual(self._run_in_chroot(
-                    ["cmake", ".", "-DCLICK_MODE=on"]), 0)
-                self.assertEqual(self._run_in_chroot(
-                    ["make", "DESTDIR=package", "install"]), 0)
-                subprocess.check_call(
-                    [self.click_binary, "build", "package"])
+            manifest = self._find_manifest(branch_dir)
+            # build it
+            self._set_arch_and_framework_from_manifest(manifest)
+            with chdir(branch_dir):
+                self._ensure_click_chroot()
+                self._configure()
+                self._make()
+                self._build_click()
                 
 
                 

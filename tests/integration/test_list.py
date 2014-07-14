6c5c67a8b6f9be85476065258c599be64179b5ec
Michael Vogt <michael.vogt@ubuntu.com>
2014-07-14 14:55:54 +0200

    add integration test for click list

---
+++ tests/integration/test_list.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2014 Canonical Ltd.
+# Author: Michael Vogt <michael.vogt@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Integration tests for the click CLI list command."""
+
+import os
+import subprocess
+
+from .helpers import ClickTestCase
+
+
+class TestList(ClickTestCase):
+    def test_list_simple(self):
+        name = "com.ubuntu.verify-ok"
+        path_to_click = self._make_click(name, framework="")
+        user = os.environ.get("USER", "root")
+        subprocess.check_call([
+            self.click_binary, "install", "--user=%s" % user,
+            path_to_click])
+        self.addCleanup(
+            subprocess.check_call,
+            [self.click_binary, "unregister", "--user=%s" % user, name])
+        output = subprocess.check_output(
+            [self.click_binary, "list", "--user=%s" % user],
+            universal_newlines=True)
+        self.assertIn(name, output)

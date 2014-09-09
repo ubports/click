4aee04dc7216e73930fb6250c120ed717c6d64b7
Michael Vogt <michael.vogt@ubuntu.com>
2014-09-09 09:56:17 +0200

    debian/tests/run-tests.sh: use the right configure parameters

--- debian/tests/run-tests.sh
+++ debian/tests/run-tests.sh
@@ -4,6 +4,9 @@ set -e
 
 # some files like config.py are generated from config.py.in
 ./autogen.sh
-./configure
+./configure --prefix=/usr \
+        --sysconfdir=/etc \
+        --with-systemdsystemunitdir=/lib/systemd/system \
+        --with-systemduserunitdir=/usr/lib/systemd/user
 
 TEST_INTEGRATION=1 python3 -m unittest discover -vv click.tests.integration
